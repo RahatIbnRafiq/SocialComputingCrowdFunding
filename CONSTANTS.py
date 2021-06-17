cba6dc36cacdcdbbb43bb1676ddaa6d9196c00f5
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2021-06-17 10:28:41 -0400

    delete extraneous files

--- CONSTANTS.py
+++
@@ -1,9 +0,0 @@
-#CHROME_DRIVER_PATH = "/Users/rahatibnrafiq/chromedriver"
-CHROME_DRIVER_PATH = "/usr/bin/chromedriver"
-URL_ADDRESS = "https://www.gofundme.com/discover/"
-CATEGORIES = ["memorial-fundraiser", "emergency-fundraiser", "charity-fundraiser",
-                  "education-fundraiser", "animal-fundraiser", "medical-fundraiser",
-                  "environment-fundraiser", "business-fundraiser", "community-fundraiser",
-                  "competition-fundraiser", "creative-fundraiser", "event-fundraiser",
-                  "faith-fundraiser", "family-fundraiser", "sports-fundraiser",
-                  "travel-fundraiser", "volunteer-fundraiser", "wishes-fundraiser"]
